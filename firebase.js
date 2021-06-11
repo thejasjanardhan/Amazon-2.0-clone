8e96095b09db83319b2012c81251f88c98a431f4
thejasjanardhan <thejasj435@gmail.com>
2021-06-11 21:57:49 +0530

    bug fix

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoRJCaGKCXsr9uWZwkyVoYzLuUeIxWDfA",
-  authDomain: "tinder-clone-3f1b9.firebaseapp.com",
-  databaseURL: "https://tinder-clone-3f1b9.firebaseio.com",
-  projectId: "tinder-clone-3f1b9",
-  storageBucket: "tinder-clone-3f1b9.appspot.com",
-  messagingSenderId: "975661845145",
-  appId: "1:975661845145:web:a7056a4d854d4abc2f0608",
+  apiKey: "AIzaSyCm5ZfZN92Uiki_1ijFnlQPdb5dTpF9ZBk",
+  authDomain: "cln-f570d.firebaseapp.com",
+  projectId: "cln-f570d",
+  storageBucket: "cln-f570d.appspot.com",
+  messagingSenderId: "1020706268571",
+  appId: "1:1020706268571:web:d0f1ed2c85c4f9b367ed3e",
 };
 
 const app = !firebase.apps.length
